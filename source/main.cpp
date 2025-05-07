0c0db10d9dce3ef5afe8b86de6548f986f1f81cc
chernenko <chernenko@protei-lab.ru>
2025-05-08 02:23:01 +0300

    Добавил тестовые файлы и скелет main'а

--- source/main.cpp
+++ source/main.cpp
@@ -2,5 +2,12 @@
 #include "file_reader.h"
 
 int main() {
+    file_reader_t fr({"test_input/file_1, test_input/file_2"}, {"test_output/file_1, test_output/file_2"});
+
+    if (!fr.read_files())
+        return 1;
+    if (!fr.write_files())
+        return 2;
+
     return 0;
 }
