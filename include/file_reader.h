62f3dde9f76d5f67fa0cf81b4082459d0aab1d44
chernenko <chernenko@protei-lab.ru>
2025-05-08 02:22:03 +0300

    Написал очень условный интерфейс для работы с файлами

--- include/file_reader.h
+++ include/file_reader.h
@@ -1,5 +1,16 @@
 #pragma once
 
 #include "flight_code.h"
+#include <vector>
+#include <unordered_set>
 
-class file_reader_t;
+class file_reader_t {
+public:
+    file_reader_t(const std::vector<std::string>& input_files, const std::vector<std::string>& output_files);
+    bool read_files();
+    bool write_files();
+
+private:
+    std::vector<std::string> input_files;
+    std::vector<std::string> output_files;
+};
--- source/file_reader.cpp
+++ source/file_reader.cpp
@@ -1,1 +1,8 @@
 #include "file_reader.h"
+
+file_reader_t::file_reader_t(const std::vector<std::string>& input_files, const std::vector<std::string>& output_files) :
+    input_files(input_files), output_files(output_files) {}
+
+bool file_reader_t::read_files() { return false; }
+
+bool file_reader_t::write_files() { return false; }
