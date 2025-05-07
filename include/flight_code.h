a062fab4fe9ff4023d022e83890838bbbb595cab
chernenko <chernenko@protei-lab.ru>
2025-05-08 02:21:14 +0300

    Написал прототип интерфейса для работы с кодом полета

--- include/flight_code.h
+++ include/flight_code.h
@@ -1,3 +1,21 @@
 #pragma once
 
-class flight_code_t;
+#include <string>
+#include <array>
+
+bool compare_flight_codes(const std::string& s_1, const std::string& s_2);
+
+class flight_code_t {
+public:
+    bool operator==(const flight_code_t& other) const;
+    bool parse_with_cctype(const std::string& s);
+    bool parse_with_regex(const std::string& s);
+
+public:
+    std::array<char, 4> get_company_code() const;
+    uint32_t get_flight_num() const;
+
+private:
+    std::array<char, 4> company_code;
+    uint32_t flight_num;
+};
--- source/flight_code.cpp
+++ source/flight_code.cpp
@@ -1,1 +1,16 @@
+#include <regex>
 #include "flight_code.h"
+
+bool compare_flight_codes(const std::string& s_1, const std::string& s_2) { return false; }
+
+
+bool flight_code_t::operator==(const flight_code_t& other) const { return false; }
+
+bool flight_code_t::parse_with_cctype(const std::string& s) { return false; }
+
+bool flight_code_t::parse_with_regex(const std::string& s) { return false; }
+
+
+std::array<char, 4> flight_code_t::get_company_code() const { return company_code; }
+
+uint32_t flight_code_t::get_flight_num() const { return flight_num; }
